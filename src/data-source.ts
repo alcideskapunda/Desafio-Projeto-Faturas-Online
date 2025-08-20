5d13333d381fe692c813941a5a0855f9af0a1aa1
alcideskapunda <alcideskapunda22@gmail.com>
2025-08-20 00:43:36 -0300

    feat: add data source

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import 'dotenv/config';
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DEVELOPMENT,
+    port: 5432,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    synchronize: true,
+    logging: false,
+    entities: [],
+    migrations: [],
+    subscribers: [],
+})
