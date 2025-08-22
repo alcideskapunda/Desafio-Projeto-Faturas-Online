22921f6e5a01e1eeb4e0cebae6462bfd0df0debc
alcideskapunda <alcideskapunda22@gmail.com>
2025-08-22 14:20:16 -0300

    Creating the POST /invoices endpoint

---
+++ src/routes/invoices-routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { InvoicesController } from "../controller/invoices-controller";
+
+const routes = Router()
+const controller = new InvoicesController()
+
+routes.post("/", controller.create)
+
+
+export default routes;
