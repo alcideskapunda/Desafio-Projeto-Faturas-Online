eed91d4af6b5ca63a57a973f57afd11abd5a77b5
alcideskapunda <alcideskapunda22@gmail.com>
2025-08-24 09:19:47 -0300

    feat: add PATCH/ endpoint route

--- src/routes/invoices-routes.ts
+++ src/routes/invoices-routes.ts
@@ -8,5 +8,6 @@ routes.post("/", controller.create)
 routes.get("/", controller.findAll)
 routes.get("/:id", controller.findOne)
 routes.delete("/:id", controller.remove)
+routes.patch("/:id", controller.update)
 
 export default routes;
