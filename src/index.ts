b3909dc224aa96dec44c346cdd48238fb962070e
alcideskapunda <alcideskapunda22@gmail.com>
2025-08-25 16:59:36 -0300

    feat: add cron job when starting the application

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import "dotenv/config"
 import 'reflect-metadata';
 import { app } from "./app";
 import { AppDataSource } from "./data-source";
+import cron from "node-cron";
+import { invoiceStatusUpdate } from "./util/invoice-status-update";
 
 async function main() {
     await AppDataSource.initialize();
     console.log("DataSource initialized");
 
     const port = process.env.PORT;
+
+    cron.schedule("30 * * * *", async () => {
+        await invoiceStatusUpdate()
+    })
 
     app.listen(port, () => {
         console.log(`app runnig at port:${port}`);
